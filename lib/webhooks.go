93a6d32c8ccc54b04cadbdd5cced631c8eb31643
Ingo <roessner@infai.org>
2019-08-05 11:31:38 +0200

    add timeout to connectivity test

--- lib/webhooks.go
+++ lib/webhooks.go
@@ -413,7 +413,10 @@ func InitWebhooks(config Config, connector *platform_connector_lib.Connector, lo
 		ticker := time.NewTicker(1 * time.Minute)
 		for t := range ticker.C {
 			log.Println("DEBUG: connectivity test: " + t.String())
-			resp, err := http.Post("http://localhost:"+config.WebhookPort+"/health", "application/json", bytes.NewBuffer([]byte("local connection test: "+t.String())))
+			client := http.Client{
+				Timeout: 5 * time.Second,
+			}
+			resp, err := client.Post("http://localhost:"+config.WebhookPort+"/health", "application/json", bytes.NewBuffer([]byte("local connection test: "+t.String())))
 			if err != nil {
 				log.Fatal("FATAL: connection test:", err)
 			}
