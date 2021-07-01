da0dd1d278761f0215a3d21f9ac957f68ed7c1e2
Ingo <roessner@infai.org>
2021-07-01 13:49:09 +0200

    benchmark with multiple configs

--- test/benchmark_test.go
+++ test/benchmark_test.go
@@ -30,120 +30,135 @@ import (
 )
 
 func BenchmarkPublish(b *testing.B) {
-	ctx, cancel := context.WithCancel(context.Background())
-	defer time.Sleep(10 * time.Second) //wait for container shutdown
-	defer cancel()
-
-	config, err := configuration.LoadConfig("../config.json")
+	configSync, err := configuration.LoadConfig("../config.json")
 	if err != nil {
 		b.Error(err)
 		return
 	}
-	config.Debug = false
-	config.FatalKafkaError = false
-	config.Validate = true
-	config.ValidateAllowUnknownField = true
-	config.ValidateAllowMissingField = true
-	config.Log = "stdout"
-	config.SyncKafka = true
-	config.SyncKafkaIdempotent = true
+	configSync.Debug = false
+	configSync.FatalKafkaError = false
+	configSync.Validate = true
+	configSync.ValidateAllowUnknownField = true
+	configSync.ValidateAllowMissingField = true
+	configSync.Log = "stdout"
+	configSync.SyncKafka = true
+	configSync.SyncKafkaIdempotent = false
 
-	config, err = server.New(ctx, config)
-	if err != nil {
-		b.Error(err)
-		return
-	}
+	b.Run("sync", benchmarkPublish(configSync))
 
-	testCharacteristicName := "test2"
+	var configSyncIdempotent configuration.ConfigStruct = *configSync
+	configSyncIdempotent.SyncKafkaIdempotent = true
+	b.Run("sync idempotent", benchmarkPublish(&configSyncIdempotent))
 
-	_, err = createCharacteristic("test1", config)
-	if err != nil {
-		b.Error(err)
-		return
-	}
-	characteristicId, err := createCharacteristic(testCharacteristicName, config)
-	if err != nil {
-		b.Error(err)
-		return
-	}
-	deviceTypeId, _, _, err := createDeviceType(config, config.DeviceManagerUrl, characteristicId)
-	if err != nil {
-		b.Error(err)
-		return
-	}
+	var configAsync configuration.ConfigStruct = *configSync
+	configAsync.SyncKafkaIdempotent = false
+	configAsync.SyncKafka = false
+	b.Run("async", benchmarkPublish(&configAsync))
+}
 
-	//time.Sleep(10 * time.Second)
+func benchmarkPublish(originalConfig configuration.Config) func(b *testing.B) {
+	return func(b *testing.B) {
+		ctx, cancel := context.WithCancel(context.Background())
+		defer time.Sleep(10 * time.Second) //wait for container shutdown
+		defer cancel()
 
-	c, err := client.New(config.MqttBroker, config.DeviceManagerUrl, config.DeviceRepoUrl, config.AuthEndpoint, "sepl", "sepl", "", "testname", []client.DeviceRepresentation{
-		{
-			Name:    "test1",
-			Uri:     "test1",
-			IotType: deviceTypeId,
-		},
-	})
-	if err != nil {
-		b.Error(err)
-		return
-	}
+		config, err := server.New(ctx, originalConfig)
+		if err != nil {
+			b.Error(err)
+			return
+		}
 
-	defer c.Stop()
+		testCharacteristicName := "test2"
 
-	options := paho.NewClientOptions().
-		SetUsername(config.AuthClientId).
-		SetAutoReconnect(true).
-		SetCleanSession(true).
-		AddBroker(config.MqttBroker)
-	listener := paho.NewClient(options)
-	if token := listener.Connect(); token.Wait() && token.Error() != nil {
-		log.Println("Error on listener.Connect(): ", token.Error())
-		b.Error(token.Error())
-		return
-	}
+		_, err = createCharacteristic("test1", config)
+		if err != nil {
+			b.Error(err)
+			return
+		}
+		characteristicId, err := createCharacteristic(testCharacteristicName, config)
+		if err != nil {
+			b.Error(err)
+			return
+		}
+		deviceTypeId, _, _, err := createDeviceType(config, config.DeviceManagerUrl, characteristicId)
+		if err != nil {
+			b.Error(err)
+			return
+		}
 
-	events := []string{}
-	mux := sync.Mutex{}
-	doneTime := 2 * time.Second
-	ticker := time.NewTicker(20 * time.Second)
-	token := listener.Subscribe("event/test1/sepl_get", 1, func(client paho.Client, message paho.Message) {
-		//log.Println("event:", string(message.Payload()))
+		//time.Sleep(10 * time.Second)
+
+		c, err := client.New(config.MqttBroker, config.DeviceManagerUrl, config.DeviceRepoUrl, config.AuthEndpoint, "sepl", "sepl", "", "testname", []client.DeviceRepresentation{
+			{
+				Name:    "test1",
+				Uri:     "test1",
+				IotType: deviceTypeId,
+			},
+		})
+		if err != nil {
+			b.Error(err)
+			return
+		}
+
+		defer c.Stop()
+
+		options := paho.NewClientOptions().
+			SetUsername(config.AuthClientId).
+			SetAutoReconnect(true).
+			SetCleanSession(true).
+			AddBroker(config.MqttBroker)
+		listener := paho.NewClient(options)
+		if token := listener.Connect(); token.Wait() && token.Error() != nil {
+			log.Println("Error on listener.Connect(): ", token.Error())
+			b.Error(token.Error())
+			return
+		}
+
+		events := []string{}
+		mux := sync.Mutex{}
+		doneTime := 2 * time.Second
+		ticker := time.NewTicker(20 * time.Second)
+		token := listener.Subscribe("event/test1/sepl_get", 1, func(client paho.Client, message paho.Message) {
+			//log.Println("event:", string(message.Payload()))
+			mux.Lock()
+			defer mux.Unlock()
+			events = append(events, string(message.Payload()))
+			ticker.Reset(doneTime)
+		})
+		if token.Wait() && token.Error() != nil {
+			log.Println("Error on listener.Subscribe(): ", token.Error())
+			b.Error(token.Error())
+			return
+		}
+
+		b.ResetTimer()
+		b.Run("send event", func(b *testing.B) {
+			for i := 0; i < b.N; i++ {
+				err = c.SendEvent("test1", "sepl_get", map[platform_connector_lib.ProtocolSegmentName]string{"metrics": `{"level": 42, "title": "event", "updateTime": 0}`})
+				if err != nil {
+					log.Println("ERROR:", err)
+					b.Error(err)
+					return
+				}
+			}
+		})
+
+		b.Run("send event large", func(b *testing.B) {
+			large := `{"time":"2021-02-01T05:00:03.280Z","location_ec-generator_gesamtwirkleistung":2207.443127441406,"location_ec-gesamt_gesamtwirkleistung":3958.2597578125,"location_ec-prozess_gesamtwirkleistung":3271.347515625,"location_ec-roboter_gesamtwirkleistung":600.3756289062501,"location_roboter-ausgabe_gesamtwirkleistung":662.5674282226562,"location_roboter-eingabe_gesamtwirkleistung":344.682021484375,"location_transport-gesamt_gesamtwirkleistung":1605.263171875,"location_wm1-gesamt_gesamtwirkleistung":9470.428890625,"location_wm1-heizung-reinigen_gesamtwirkleistung":2395.017423828125,"location_wm1-heizung-trocknung_gesamtwirkleistung":0.0,"location_wm2-gesamt_gesamtwirkleistung":24943.22471875,"location_wm2-heizung-reinigen_gesamtwirkleistung":7294.0812421875,"location_wm2-heizung-trocknung_gesamtwirkleistung":0.0,"location_wm2-vakuumpumpe_gesamtwirkleistung":5473.771453125001,"module_1_state":3,"module_1_errorcode":0,"module_1_errorindex":0,"module_2_state":3,"module_2_errorcode":0,"module_2_errorindex":0,"module_4_state":3,"module_4_errorcode":0,"module_4_errorindex":0,"module_5_state":3,"module_5_errorcode":0,"module_5_errorindex":0,"module_6_state":3,"module_6_errorcode":0,"module_6_errorindex":0,"module_1_processdata":[{"station":1,"process":1,"part":1,"main_id":"de6ccf143313332ab0c430504dbbd8d076458bc0a51a9fb3c36c4dce65ce3d0b","errorcode":0,"result":null},{"station":1,"process":1,"part":2,"main_id":"41361e5961b88590b2dee2f396e72406e8b5d7749e320bc982f65f8f6674db26","errorcode":0,"result":null}],"module_2_processdata":null}`
+			for i := 0; i < b.N; i++ {
+				err = c.SendEvent("test1", "sepl_get", map[platform_connector_lib.ProtocolSegmentName]string{"metrics": large})
+				if err != nil {
+					log.Println("ERROR:", err)
+					b.Error(err)
+					return
+				}
+			}
+		})
+
+		<-ticker.C
+		ticker.Stop()
 		mux.Lock()
 		defer mux.Unlock()
-		events = append(events, string(message.Payload()))
-		ticker.Reset(doneTime)
-	})
-	if token.Wait() && token.Error() != nil {
-		log.Println("Error on listener.Subscribe(): ", token.Error())
-		b.Error(token.Error())
-		return
+		b.Log(len(events))
 	}
-
-	b.ResetTimer()
-	b.Run("send event", func(b *testing.B) {
-		for i := 0; i < b.N; i++ {
-			err = c.SendEvent("test1", "sepl_get", map[platform_connector_lib.ProtocolSegmentName]string{"metrics": `{"level": 42, "title": "event", "updateTime": 0}`})
-			if err != nil {
-				log.Println("ERROR:", err)
-				b.Error(err)
-				return
-			}
-		}
-	})
-
-	b.Run("send event large", func(b *testing.B) {
-		large := `{"time":"2021-02-01T05:00:03.280Z","location_ec-generator_gesamtwirkleistung":2207.443127441406,"location_ec-gesamt_gesamtwirkleistung":3958.2597578125,"location_ec-prozess_gesamtwirkleistung":3271.347515625,"location_ec-roboter_gesamtwirkleistung":600.3756289062501,"location_roboter-ausgabe_gesamtwirkleistung":662.5674282226562,"location_roboter-eingabe_gesamtwirkleistung":344.682021484375,"location_transport-gesamt_gesamtwirkleistung":1605.263171875,"location_wm1-gesamt_gesamtwirkleistung":9470.428890625,"location_wm1-heizung-reinigen_gesamtwirkleistung":2395.017423828125,"location_wm1-heizung-trocknung_gesamtwirkleistung":0.0,"location_wm2-gesamt_gesamtwirkleistung":24943.22471875,"location_wm2-heizung-reinigen_gesamtwirkleistung":7294.0812421875,"location_wm2-heizung-trocknung_gesamtwirkleistung":0.0,"location_wm2-vakuumpumpe_gesamtwirkleistung":5473.771453125001,"module_1_state":3,"module_1_errorcode":0,"module_1_errorindex":0,"module_2_state":3,"module_2_errorcode":0,"module_2_errorindex":0,"module_4_state":3,"module_4_errorcode":0,"module_4_errorindex":0,"module_5_state":3,"module_5_errorcode":0,"module_5_errorindex":0,"module_6_state":3,"module_6_errorcode":0,"module_6_errorindex":0,"module_1_processdata":[{"station":1,"process":1,"part":1,"main_id":"de6ccf143313332ab0c430504dbbd8d076458bc0a51a9fb3c36c4dce65ce3d0b","errorcode":0,"result":null},{"station":1,"process":1,"part":2,"main_id":"41361e5961b88590b2dee2f396e72406e8b5d7749e320bc982f65f8f6674db26","errorcode":0,"result":null}],"module_2_processdata":null}`
-		for i := 0; i < b.N; i++ {
-			err = c.SendEvent("test1", "sepl_get", map[platform_connector_lib.ProtocolSegmentName]string{"metrics": large})
-			if err != nil {
-				log.Println("ERROR:", err)
-				b.Error(err)
-				return
-			}
-		}
-	})
-
-	<-ticker.C
-	ticker.Stop()
-	mux.Lock()
-	defer mux.Unlock()
-	b.Log(len(events))
 }
