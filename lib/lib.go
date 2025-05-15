8fabc7fa61815fe8faf2bb913183383923411d3e
y-du <42994541+y-du@users.noreply.github.com>
2025-05-15 13:43:35 +0200

    fix error handling

--- lib/lib.go
+++ lib/lib.go
@@ -156,12 +156,16 @@ func Start(ctx context.Context, config configuration.Config) (err error) {
 			httpTimeout = tmp
 		}
 		logger, err = connectionlog.NewWithProducerAndConnCheck(logProducer, connection_check_lib.New(&http.Client{Timeout: httpTimeout}, config.ConnectionCheckUrl), config.DeviceLogTopic, config.GatewayLogTopic)
+		if err != nil {
+			log.Println("ERROR: logger ", err)
+			return err
+		}
 	} else {
 		logger, err = connectionlog.NewWithProducer(logProducer, config.DeviceLogTopic, config.GatewayLogTopic)
-	}
-	if err != nil {
-		log.Println("ERROR: logger ", err)
-		return err
+		if err != nil {
+			log.Println("ERROR: logger ", err)
+			return err
+		}
 	}
 
 	m, err := metrics.NewMetrics()
