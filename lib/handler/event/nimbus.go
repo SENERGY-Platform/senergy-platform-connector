b82f827013da6a9c3f19b6dbfabf1ad096b0f541
franzmueller <franzmueller@users.noreply.github.com.>
2025-10-15 14:48:47 +0200

    wmbus: fix device type update detection

--- lib/handler/event/nimbus.go
+++ lib/handler/event/nimbus.go
@@ -193,7 +193,7 @@ func (this *Handler) ensureWmbusDeviceType(deviceTypeId string, msg model.Encryp
 
 	deviceType = util.DeviceType(deviceTypeId, msg.Manufacturer, msg.Type, msg.Version, this.config.WmbusDeviceClassId, this.config.SenergyProtocolId, this.config.SenergyProtoclSegment, decoded)
 
-	if len(existingDeviceType.Services) == 0 || len(existingDeviceType.Services[0].Outputs) == 0 || len(existingDeviceType.Services[0].Outputs[0].ContentVariable.SubContentVariables) < len(deviceType.Services[0].Outputs[0].ContentVariable.SubContentVariables) {
+	if len(existingDeviceType.Services) == 0 || len(existingDeviceType.Services[0].Outputs) == 0 || (len(deviceType.Services) > 0 && len(deviceType.Services[0].Outputs) > 0 && len(existingDeviceType.Services[0].Outputs[0].ContentVariable.SubContentVariables) < len(deviceType.Services[0].Outputs[0].ContentVariable.SubContentVariables)) {
 		deviceType, err = this.connector.IotCache.UpdateDeviceType(adminToken, deviceType)
 		return deviceType, err
 	} else {
